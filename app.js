d2d2210d8f2d5f223bb4c69ef2073fc7494997a0
Anaghaa Patil <144719631+Anaa1101@users.noreply.github.com>
2026-04-02 12:55:26 +0530

    Update secret key in app.js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const password = "admin123";
 const apiKey = "sk-secret-key-12345";
 const db = require('mongodb');
 db.collection.find({user: req.body.username});
-const secret = "supersecret1257786734";
+const secret = "supersecret12577864";
